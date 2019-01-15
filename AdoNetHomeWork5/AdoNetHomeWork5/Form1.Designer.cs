4145ef75c47dbae177cbf8885e6f8af7b618ce2e
Олжас Жаксылыков <40593443+ZhaxylykovOlzhas@users.noreply.github.com>
2019-01-15 17:52:30 +0600

    Add files via upload

---
+++ AdoNetHomeWork5/AdoNetHomeWork5/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AdoNetHomeWork5
+{
+	partial class Form1
+	{
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Enabled = false;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 75);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(831, 484);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(320, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(225, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(390, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Таблицы";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(887, 571);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ AdoNetHomeWork5/AdoNetHomeWork5/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace AdoNetHomeWork5
+{
+	public partial class Form1 : Form
+	{
+		SqlConnection connect = new SqlConnection();
+		SqlCommand command = new SqlCommand();
+		SqlDataReader reader;
+		string connection = null;
+
+		public Form1()
+		{
+			connection = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+			InitializeComponent();
+
+			connect = new SqlConnection(connection);
+			string Select = "select table_name from information_schema.tables where table_type= 'base table' and table_name not like 'sys%';";
+
+			try
+			{
+				connect.Open();
+				command = new SqlCommand(Select, connect);
+				reader = command.ExecuteReader();
+				while (reader.Read())
+				{
+					comboBox1.Items.Add(reader["table_name"]);
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			connect?.Close();
+			command?.Clone();
+
+		}
+
+
+
+		private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+		{
+			SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("Select * from " + comboBox1.Text, connect);
+			DataSet data = new DataSet();
+			sqlDataAdapter.Fill(data);
+			dataGridView1.DataSource = data.Tables[0];
+		}
+
+	}
+}
